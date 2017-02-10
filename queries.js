e0773b02a1c669551ba515547b1eb0ed2f2e748d
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-02-10 23:10:26 +0000

    change payload structure

--- queries.js
+++ queries.js
@@ -1,15 +1,12 @@
 var options = {};
 var pgp = require('pg-promise')(options);
-var db = pgp(process.env.DATABASE_URL);
+var db = pgp(process.env.DATABASE_URL + '?ssl=true');
 
 function getAllEntries(req, res, next) {
   db.any('SELECT * FROM entries')
     .then(function (data) {
       res.status(200)
-        .json({
-          status: 'success',
-          data: data
-        });
+        .json(data);
     })
     .catch(function (err) {
       return next(err);
@@ -21,10 +18,7 @@ function getEntry(req, res, next) {
   db.one('SELECT * FROM entries WHERE id = $1', entryId)
     .then(function (data) {
       res.status(200)
-        .json({
-          status: 'success',
-          data: data
-        });
+        .json(data);
     })
     .catch(function (err) {
       return next(err);
