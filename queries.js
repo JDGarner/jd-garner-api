5464238e9340774ef2d28790e6e4383fe5ca0fa2
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-02-10 22:38:36 +0000

    test db env config change

--- queries.js
+++ queries.js
@@ -1,7 +1,6 @@
 var options = {};
 var pgp = require('pg-promise')(options);
-// var process.env.DATABASE_URL = 'postgres://plzqpnntbrelog:eb0b29419fe5a8a2515464a914ef1a856db0fde657291147a60473925b1ef8b2@ec2-107-22-236-252.compute-1.amazonaws.com:5432/ddme08r6htmcqs';
-var db = pgp(process.env.DATABASE_URL + '?ssl=true');
+var db = pgp(process.env.DATABASE_URL);
 
 function getAllEntries(req, res, next) {
   db.any('SELECT * FROM entries')
